2be5272e423d5cfb987b5f930520e38bb51cd67f
784287979@qq.com <784287979@qq.com>
2025-11-22 19:24:54 +0800

    23132132132131

--- gre_codes/C_project/C_project/C016_mem.c
+++ gre_codes/C_project/C_project/C016_mem.c
@@ -3,11 +3,14 @@
 #include<string.h>
 int main(void) {
 	int arr[] = { 1,2,3 };
+	int arr2[] = { 1,2,3 };
 	int* my = (int*)malloc(sizeof(int) * 3);
 	memcpy(my, arr, sizeof(int) * 3);
+	memmov(my, arr, sizeof(int) * 3);
 	for (int i = 0; i < 3; i++) {
 		printf("%d\n", my[i]);
 	}
+	//int res = memcmp(arr, arr2);
 
 	/*int* p = (int *)malloc(sizeof(int) * 10);
 	memset(p, 0, 40);
