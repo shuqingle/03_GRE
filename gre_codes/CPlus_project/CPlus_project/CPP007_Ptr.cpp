cc88c69647c78d0db164093de659853bb8da36d3
784287979@qq.com <784287979@qq.com>
2025-12-01 22:43:48 +0800

    32132132132

---
+++ gre_codes/CPlus_project/CPlus_project/CPP007_Ptr.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(void) {
+	int a = 10;
+	int* ptr;
+	ptr = &a;
+
+	cout << &a << endl;
+	cout << ptr << endl;
+	cout << sizeof(ptr) << endl;
+	cout << sizeof(char*) << endl;
+	int* p = NULL;
+	//cout << *p << endl;
+	cout << p << endl;
+	
+	int* p2 = (int*)0x22110011;
+	cout << *p2 << endl;
+	cout << p2 << endl;
+	return 0;
+}
